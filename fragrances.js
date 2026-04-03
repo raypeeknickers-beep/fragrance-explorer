67928422408167000a89c74cd61acb950145c32e
raypeeknickers-beep <raypeeknickers@gmail.com>
2026-04-03 22:53:26 +0100

    Update fragrances.js

--- fragrances.js
+++ fragrances.js
@@ -1,29 +1,81 @@
-// data/fragrances.js
 export const fragrances = [
-    {
-        id: 1,
-        name: "Dior Sauvage",
-        brand: "Dior",
-        image: "https://images.clothes.com/path-to-sauvage.png", // Use direct image URLs
-        topNotes: ["Bergamot", "Pepper"],
-        middleNotes: ["Sichuan Pepper", "Lavender", "Pink Pepper"],
-        baseNotes: ["Ambroxan", "Cedar", "Labdanum"],
-        longevity: "8-10 Hours",
-        season: "All Season",
-        occasion: "Versatile / Office",
-    },
-    // ... Repeat for 100 entries
+  {
+    id: 1,
+    name: "Sauvage",
+    brand: "Dior",
+    image: "https://www.dior.com/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master_dior/default/dw646927a3/assets/Y0996004/Y0996004_E01.jpg",
+    topNotes: ["Calabrian Bergamot", "Pepper"],
+    middleNotes: ["Sichuan Pepper", "Lavender", "Pink Pepper", "Vetiver"],
+    baseNotes: ["Ambroxan", "Cedar", "Labdanum"],
+    longevity: "8-10 Hours",
+    season: "All Year",
+    occasion: "Daily Wear"
+  },
+  {
+    id: 2,
+    name: "Aventus",
+    brand: "Creed",
+    image: "https://fimgs.net/mdimg/perfume/m.9828.jpg",
+    topNotes: ["Pineapple", "Bergamot", "Black Currant", "Apple"],
+    middleNotes: ["Birch", "Patchouli", "Moroccan Jasmine", "Rose"],
+    baseNotes: ["Musk", "Oak Moss", "Ambergris", "Vanille"],
+    longevity: "10-12 Hours",
+    season: "Spring/Summer",
+    occasion: "Success / Special Events"
+  },
+  {
+    id: 3,
+    name: "Bleu de Chanel",
+    brand: "Chanel",
+    image: "https://www.chanel.com/images/t_one/t_fragrance_v2/q_auto:good,f_auto,fl_lossy,dpr_1.1/w_620/bleu-de-chanel-eau-de-parfum-spray-100ml-v3-packshot-107360-9533350281246.jpg",
+    topNotes: ["Grapefruit", "Lemon", "Mint", "Pink Pepper"],
+    middleNotes: ["Ginger", "Nutmeg", "Jasmine", "Iso E Super"],
+    baseNotes: ["Incense", "Vetiver", "Cedar", "Sandalwood"],
+    longevity: "7-9 Hours",
+    season: "All Year",
+    occasion: "Office / Date Night"
+  },
+  {
+    id: 4,
+    name: "Eros",
+    brand: "Versace",
+    image: "https://images.clothes.com/path-to-eros.png",
+    topNotes: ["Mint", "Green Apple", "Lemon"],
+    middleNotes: ["Tonka Bean", "Ambroxan", "Geranium"],
+    baseNotes: ["Madagascar Vanilla", "Virginian Cedar", "Vetiver"],
+    longevity: "10+ Hours",
+    season: "Winter/Fall",
+    occasion: "Clubbing / Night Out"
+  },
+  {
+    id: 5,
+    name: "Le Male Elixir",
+    brand: "Jean Paul Gaultier",
+    image: "https://fimgs.net/mdimg/perfume/m.79350.jpg",
+    topNotes: ["Lavender", "Mint"],
+    middleNotes: ["Vanilla", "Benzoin"],
+    baseNotes: ["Honey", "Tonka Bean", "Tobacco"],
+    longevity: "12+ Hours",
+    season: "Winter",
+    occasion: "Date Night"
+  }
+  // Keep adding until you hit 100!
 ];
 
 export const creators = [
-    {
-        name: "Jeremy Fragrance",
-        desc: "The number one fragrance icon. Known for his 'Power' and 'Strength' mantra.",
-        bgImage: "https://example.com/jeremy-bg.jpg"
-    },
-    {
-        name: "TheCologneBoy",
-        desc: "Specializing in youthful, mass-appealing scents and street interviews.",
-        bgImage: "https://example.com/cologneboy-bg.jpg"
-    }
+  {
+    name: "Jeremy Fragrance",
+    desc: "The world's most followed fragrance influencer. Known for his high energy and 'Power' rankings.",
+    bgImage: "https://yt3.googleusercontent.com/ytc/AIdro_n_Y5V5V6V..." 
+  },
+  {
+    name: "TheCologneBoy",
+    desc: "Popular for his street interviews and finding what scents people actually love on others.",
+    bgImage: "https://example.com/cologneboy.jpg"
+  },
+  {
+    name: "Noel Thomas",
+    desc: "The king of aesthetic fragrance reviews and deep dives into luxury niche perfumery.",
+    bgImage: "https://example.com/noel.jpg"
+  }
 ];
